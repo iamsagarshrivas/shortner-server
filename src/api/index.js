4b47519551abeaa0aae1f7bc88de4eedb58871c7
Sagar Shrivas <sagar.shrivas@geminisolutions.in>
2019-10-17 07:26:08 +0530

    issue fixed

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,6 @@
 var router = require('express').Router();
 var weblinksRouter = require('./weblinks');
-var { redirectURL } = require('./weblinks/weblinks.controller')
 
 router.use('/api/weblinks',weblinksRouter);
-router.get('/api/:link', redirectURL)
-router.get('/api',(req,res)=>{
-	res.send('working')
-})
 
 module.exports = router;
--- src/api/weblinks/weblinks.controller.js
+++ src/api/weblinks/weblinks.controller.js
@@ -1,5 +1,4 @@
 var {WeblinksService} = require('../../services');
-var { NOT_FOUND_URL, CLIENT_URL } = require('../../config/constants');
 module.exports = {
 	generateLink:async ({body},res)=>{
 		try{
@@ -8,16 +7,6 @@ module.exports = {
 		}catch(error){
 			console.log('[ERROR]:',error);
 			res.status(500).json({error: true, message:'Unable to process'})
-		}
-	},
-	redirectURL:async({params},res)=>{
-		var shortLink = params.link;		
-		var weblinkObj = await WeblinksService.findOneAndUpdate({shortLink: shortLink, isActive: true},{$inc:{"visitCount":1}});
-		if(weblinkObj){
-			res.redirect(301,`${CLIENT_URL}/${weblinkObj.shortLink}`)
-		}
-		else{
-			res.redirect(301,NOT_FOUND_URL)
 		}
 	},
 	originalURL: async({body},res)=>{		
